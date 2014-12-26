3cadff8aee3e71d3e825efae050afe9233e59e2b
JonyEpsilon <github@j-star.org>
2014-12-26 15:27:29 +0000

    Release 0.1.0.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
 ;;;;
 ;;;; gg4clj is licenced to you under the MIT licence. See the file LICENCE.txt for full details.
 
-(defproject gg4clj "0.1.0-SNAPSHOT"
+(defproject gg4clj "0.1.0"
   :description "A simple wrapper for R's ggplot2 in Clojure and Gorilla REPL."
   :url "https://github.com/JonyEpsilon/gg4clj"
   :license {:name "MIT"}
